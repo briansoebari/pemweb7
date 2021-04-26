c8ff71e1d4eeb99b349386007c4cc1343764e366
briansoebari <79493821+briansoebari@users.noreply.github.com>
2021-04-26 16:12:35 +0700

    Add files via upload

---
+++ admin/koneksi.php
@@ -0,0 +1,9 @@
+<?php
+// konfigurasi Database
+$host="localhost";
+$user="root";
+$password="";
+$database="Latihan";
+// perintah php untuk akses ke Database
+$koneksi=mysqli_connect($host, $user, $password, $database);
+?>
